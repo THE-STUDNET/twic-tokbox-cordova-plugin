1773bb124b4dba67c119cd43fa1f7579a2916e72
Castellani Emmanuel <emmanuel@ecaste.com>
2017-03-02 18:05:43 +0100

    init of ios plugin including cocoapods dependencies

--- src/ios/TWICCordovaPlugin.m
+++ src/ios/TWICCordovaPlugin.m
@@ -1,36 +1,30 @@
 #import "TWICCordovaPlugin.h"
 #import <Cordova/CDV.h>
+#import "SVProgressHUD.h"
 
 @implementation TWICCordovaPlugin
 
-- (void)configure:(CDVInvokedUrlCommand*)command
+- (void)show:(CDVInvokedUrlCommand*)command
 {
-  CDVPluginResult* pluginResult = nil;
-  NSString* msg = [command.arguments objectAtIndex:0];
+    CDVPluginResult* pluginResult = nil;
+    NSString* msg = [command.arguments objectAtIndex:0];
 
-  if (msg == nil || [msg length] == 0) {
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-  } else {
-    UIAlertView *toast = [
-      [UIAlertView alloc] initWithTitle:@""
-        message:msg
-        delegate:nil
-        cancelButtonTitle:nil
-        otherButtonTitles:nil, nil];
-
-    [toast show];
-
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC),
-dispatch_get_main_queue(), ^{
-      [toast dismissWithClickedButtonIndex:0 animated:YES];
-    });
-
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
-messageAsString:msg];
-  }
-
-  [self.commandDelegate sendPluginResult:pluginResult
-callbackId:command.callbackId];
+    if (msg == nil || [msg length] == 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+    else
+    {
+       [SVProgressHUD showSuccessWithStatus:msg];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+                                         messageAsString:msg];
+    }
+    [self.commandDelegate sendPluginResult:pluginResult
+                                callbackId:command.callbackId];
 }
 
+- (void)configure:(CDVInvokedUrlCommand*)command
+{
+    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK]
+                                callbackId:command.callbackId];
+}
 @end
